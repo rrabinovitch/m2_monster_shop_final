9a65aadacacb66f6b78c1339ff0be8b63355300b
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-21 15:06:48 -0600

    Update User Model to enum roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,9 @@ class User < ApplicationRecord
   validates_numericality_of :zip
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password, require: true
-  validates_numericality_of :role
+  validates_presence_of :role
 
   has_secure_password
 
+  enum role: [:regular, :merchant_employee, :admin]
 end
