4a43e4520ec6aa8a1ffbe41d4b8a8628e8781c2e
Lito White <leland.white@gmail.com>
2020-07-21 20:47:12 -0700

    User Story 13 completed for regular user and admin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,18 @@
 class SessionsController < ApplicationController
 
   def new
+  end
 
+  def create
+    user = User.find_by(email: params[:email])
+    session[:user_id] = user.id
+    flash[:success] = "You are now logged in!"
+    if user.role == "regular"
+      redirect_to '/profile'
+    elsif user.role == "merchant_employee"
+      redirect_to 'merchant/dashboard'
+    elsif user.role == "admin"
+      redirect_to '/admin/dashboard'
+    end
   end
 end
