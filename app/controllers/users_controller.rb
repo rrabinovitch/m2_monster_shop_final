ec4b45dd5bc288ff10b72d6be7f95b19cf6975a7
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-21 21:06:01 -0600

    Add feature: the new user registration form cannot be submitted with missing details
    - There is now a sad path for User#create
    - Passwords have to be checked for matching before checking user.save
    -- Otherwise, user.save will create a record in the database
    
    - The new user registration form now uses a number_field_tag
    -- This design implementation will prevent edge-cases where:
    -- A user can enter a non-number as a Zip Code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,15 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
-    if user.save
+    if passwords_match? && user.save
       flash[:success] = "You are now registered and logged in!"
       redirect_to "/profile"
+    else
+      missing_params = user_params.select { |_, param| param.empty? }
+      missing_params = missing_params.keys.join(", ")
+      missing_params = "confirm password" if missing_params.empty?
+      flash[:failure] = "Please fill in the missing fields: #{missing_params}."
+      redirect_to "/register"
     end
   end
 
@@ -19,6 +25,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.permit(:name, :address, :city, :state, :zip, :email, :password) if passwords_match?
+    params.permit(:name, :address, :city, :state, :zip, :email, :password)
   end
 end
