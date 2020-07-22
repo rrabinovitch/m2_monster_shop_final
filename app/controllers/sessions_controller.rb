0efbc0015ae9fa58bc3ca0711eb83299dabd5692
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-07-22 14:54:34 -0600

    Add sad path for bad login credentials in SessionsController#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,14 +5,19 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
-    session[:user_id] = user.id
-    flash[:success] = "You are now logged in!"
-    if user.role == "regular"
-      redirect_to '/profile'
-    elsif user.role == "merchant_employee"
-      redirect_to '/merchant/dashboard'
-    elsif user.role == "admin"
-      redirect_to '/admin/dashboard'
+    if user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "You are now logged in!"
+      if user.role == "regular"
+        redirect_to '/profile'
+      elsif user.role == "merchant_employee"
+        redirect_to '/merchant/dashboard'
+      elsif user.role == "admin"
+        redirect_to '/admin/dashboard'
+      end
+    else
+      flash[:error] = 'Wrong email or password entered - please try logging in again.'
+      render :new
     end
   end
 end
