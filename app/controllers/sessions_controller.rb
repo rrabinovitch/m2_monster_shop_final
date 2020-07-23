906e1d7e65a26b702383735b299b52276430603e
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2020-07-23 09:54:41 -0600

    Revert "Refactor Sessions Controller Actions"

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,15 @@
 class SessionsController < ApplicationController
 
   def new
-    if already_logged_in?
+    if current_user && current_user.regular?
+      redirect_to '/profile'
       flash[:notice] = "You are already logged in."
-      redirect_logged_in(current_user)
+    elsif current_user && current_user.merchant_employee?
+      redirect_to '/merchant/dashboard'
+      flash[:notice] = "You are already logged in."
+    elsif current_user && current_user.admin?
+      redirect_to '/admin/dashboard'
+      flash[:notice] = "You are already logged in."
     end
   end
 
@@ -12,38 +18,16 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       flash[:success] = "You are now logged in!"
-      redirect_logged_in(user)
+      if user.regular?
+        redirect_to '/profile'
+      elsif user.merchant_employee?
+        redirect_to '/merchant/dashboard'
+      elsif user.admin?
+        redirect_to '/admin/dashboard'
+      end
     else
       flash[:error] = 'Wrong email or password entered - please try logging in again.'
       render :new
     end
-  end
-
-  private
-
-  def redirect_logged_in(user)
-    if user.regular?
-      redirect_to '/profile'
-    elsif user.merchant_employee?
-      redirect_to '/merchant/dashboard'
-    elsif user.admin?
-      redirect_to '/admin/dashboard'
-    end
-  end
-
-  def regular_user_logged_in?
-    current_user && current_user.regular?
-  end
-
-  def merchant_employee_logged_in?
-    current_user && current_user.merchant_employee?
-  end
-
-  def admin_logged_in?
-    current_user && current_user.admin?
-  end
-
-  def already_logged_in?
-    regular_user_logged_in? || merchant_employee_logged_in? || admin_logged_in?
   end
 end
