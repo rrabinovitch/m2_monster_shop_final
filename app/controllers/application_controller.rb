754c04abc6d5ea2fea87141110e7047af084e205
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-23 13:59:10 -0600

    Memo-ize logged_in_user as an instance variable to prevent multiple database lookups

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,6 @@ class ApplicationController < ActionController::Base
   end
 
   def logged_in_user
-    User.find(session[:user_id]) if session[:user_id]
+    @logged_in_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 end
