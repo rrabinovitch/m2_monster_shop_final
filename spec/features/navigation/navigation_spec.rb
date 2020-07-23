8df6b761ea67ae7416dec4f841ee8f40260b2a6e
Judith Pillado <judith.pillado@gmail.com>
2020-07-22 21:02:50 -0500

    Add navigation to its own folder

--- spec/features/navigation/navigation_spec.rb
+++ spec/features/navigation/navigation_spec.rb
@@ -50,6 +50,30 @@ RSpec.describe 'Site Navigation' do
         expect(page).to have_content("Cart: 0")
       end
     end
-
   end
+
+  describe 'As a Regular User: I see the same links as a visitor' do
+    it "Plus a link to my profile page and a link to log out " do
+      visit "/"
+
+    end
+
+    xit "Minus a link to log in or register" do
+
+    end
+
+    xit "And I see a text that says I am logged in with my name" do
+
+    end
+  end
+#   As a default user
+# I see the same links as a visitor
+# Plus the following links
+#
+# a link to my profile page ("/profile")
+# a link to log out ("/logout")
+# Minus the following links
+#
+# I do not see a link to log in or register
+# I also see text that says "Logged in as Mike Dao" (or whatever my name is)
 end
