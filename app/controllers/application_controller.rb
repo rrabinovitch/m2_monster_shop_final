051e987db25136cacd30b2e5f015badf5cea109b
Judith Pillado <judith.pillado@gmail.com>
2020-07-23 15:20:14 -0500

    Add helper method :user to application controller. This allowed user to be used in the application view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :cart
+  helper_method :cart, :user
 
   def cart
     @cart ||= Cart.new(session[:cart] ||= Hash.new(0))
   end
 
+  def user
+    if session[:user_id] != nil 
+      @user = User.find(session[:user_id])
+    end
+  end
+
   def index
-  end 
+  end
 
 end
