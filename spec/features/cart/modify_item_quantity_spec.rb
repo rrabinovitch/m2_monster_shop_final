dfec9622713033ae48a5a1e4921b5476ee1560bf
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-23 17:29:13 -0600

    Update spec to check side effects on NavBar Cart indicator

--- spec/features/cart/modify_item_quantity_spec.rb
+++ spec/features/cart/modify_item_quantity_spec.rb
@@ -16,27 +16,36 @@ RSpec.describe "Cart show" do
 
       describe "Add Item Quantity to Cart" do
         it "I can click a button to increment the count of items I want to purchase" do
-          within "#cart-item-#{@item.id}" do
-            expect(page).to have_content("1")
-            expect(page).to have_button(@increment_button)
-
-            click_on @increment_button
-            expect(current_path).to eq(cart_path)
-            expect(page).to have_content("2")
+          within 'nav' do
+            expect(page).to have_content("Cart: 1")
           end
 
+          within "#cart-item-#{@item.id}" do
+            expect(page).to have_button(@increment_button)
+            click_on @increment_button
+          end
+
+          expect(current_path).to eq(cart_path)
+
+          within 'nav' do
+            expect(page).to have_content("Cart: 2")
+          end
         end
 
         it "I cannot increment the count beyond the item's inventory size" do
+          within 'nav' do
+            expect(page).to have_content("Cart: 1")
+          end
+
           within "#cart-item-#{@item.id}" do
-            expect(page).to have_content("1")
+            2.times { click_on @increment_button }
+          end
 
-            click_on @increment_button
-            expect(page).to have_content("2")
+          within 'nav' do
+            expect(page).to have_content("Cart: 3")
+          end
 
-            click_on @increment_button
-            expect(page).to have_content("3")
-
+          within "#cart-item-#{@item.id}" do
             expect(page).to_not have_button(@increment_button)
           end
         end
@@ -46,21 +55,31 @@ RSpec.describe "Cart show" do
         it "I can click a button to decrement the count of items I want to purchase" do
           within "#cart-item-#{@item.id}" do
             click_on @increment_button
-            expect(page).to have_content("2")
-
-            expect(page).to have_button(@decrement_button)
-
-            click_on @decrement_button
-            expect(current_path).to eq(cart_path)
-            expect(page).to have_content("1")
           end
 
+          within 'nav' do
+            expect(page).to have_content("Cart: 2")
+          end
+
+          within "#cart-item-#{@item.id}" do
+            expect(page).to have_button(@decrement_button)
+            click_on @decrement_button
+          end
+
+          expect(current_path).to eq(cart_path)
+
+          within 'nav' do
+            expect(page).to have_content("Cart: 1")
+          end
         end
 
         it "If I decrement the count to 0 the item is immediately removed from my cart" do
+          within 'nav' do
+            expect(page).to have_content("Cart: 1")
+          end
+
           within "#cart-item-#{@item.id}" do
             expect(page).to have_button(@decrement_button)
-            expect(page).to have_content("1")
             click_on @decrement_button
           end
 
