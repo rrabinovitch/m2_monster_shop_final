ffb116a21c44a5bf80c30c37b853b70763d475b6
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-07-23 00:00:06 -0600

    Define logout route

--- config/routes.rb
+++ config/routes.rb
@@ -40,6 +40,7 @@ Rails.application.routes.draw do
 
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
+  get "/logout", to: "sessions#destroy"
 
   namespace :admin do
     get "/dashboard", to: "dashboard#index"
