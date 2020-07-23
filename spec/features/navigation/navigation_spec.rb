74c0e2a66a936718310c99be30285e924f388aa1
Judith Pillado <judith.pillado@gmail.com>
2020-07-23 16:50:26 -0500

    Add User Story 4: As a Merchant Employee User

--- spec/features/navigation/navigation_spec.rb
+++ spec/features/navigation/navigation_spec.rb
@@ -10,6 +10,15 @@ RSpec.describe 'Site Navigation' do
                         email: "janedoe@email.com",
                         password: "password",
                         password_confirmation: "password")
+    @merchant1 = User.create(name: "Megan Stang",
+                                 address: "456 Cool St",
+                                 city: "Denver",
+                                 state: "CO",
+                                 zip: 80203,
+                                 email: "megstang@email.com",
+                                 password: "password",
+                                 password_confirmation: "password",
+                                 role: 1)
   end
 
   describe 'As a Visitor' do
@@ -63,7 +72,6 @@ RSpec.describe 'Site Navigation' do
 
   describe 'As a Regular User: I see the same links as a visitor' do
     it "Plus a link to my profile page and a link to log out, but no links to log in or register" do
-
       visit '/login'
       fill_in :email, with: @user.email
       fill_in :password, with: "password"
@@ -71,6 +79,23 @@ RSpec.describe 'Site Navigation' do
       expect(current_path).to eq("/profile")
       expect(page).to have_content("Logged in as #{@user.name}")
       expect(page).to have_link("Log Out")
+      expect(page).to_not have_link("Log In")
+      expect(page).to_not have_link("Register")
+    end
+  end
+
+  describe 'As a Merchant Employee: I see the same links as a Regular Visitor' do
+    it "Plus a link to my merchant dashboard" do
+      visit '/login'
+      fill_in :email, with: @merchant1.email
+      fill_in :password, with: "password"
+      click_button 'Log In'
+      expect(current_path).to eq("/merchant/dashboard")
+      expect(page).to have_content("Logged in as #{@merchant1.name}")
+      expect(page).to have_link("My Dashboard")
+      expect(page).to have_link("Log Out")
+      expect(page).to_not have_link("Log In")
+      expect(page).to_not have_link("Register")
     end
   end
 
