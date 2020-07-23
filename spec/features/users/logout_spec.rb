996d942a5686536decb7c16443ebd416854c8cbe
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-07-22 23:41:30 -0600

    Set up user story 16 tests for user logout functionality

---
+++ spec/features/users/logout_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'User can log out' do
+  describe "When I visit the logout path" do
+    it "I am redirected to the site's home page, I see a flash message indicating I'm logged out, and my shopping cart is emptied" do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit '/logout'
+      expect(current_path).to eq('/')
+      expect(page).to have_content("You have been logged out.")
+      within('.topnav') do
+        expect(page).to have_content("Cart: 0")
+      end
+    end
+  end
+end
