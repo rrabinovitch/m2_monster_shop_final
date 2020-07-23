4ccd039f5c882463cc741542425076d6ea6df349
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-07-23 00:41:49 -0600

    Add cart setup to test

--- spec/features/users/logout_spec.rb
+++ spec/features/users/logout_spec.rb
@@ -6,6 +6,13 @@ RSpec.describe 'User can log out' do
       user = create(:user)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
+      item = create(:item)
+      visit "/items/#{item.id}"
+      click_on "Add To Cart"
+      within('.topnav') do
+        expect(page).to have_content("Cart: 1")
+      end
+
       visit '/logout'
       expect(current_path).to eq('/')
       expect(page).to have_content("You have been logged out.")
