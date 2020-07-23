801fda8f8ec23e5d4eed3c3901ca03af0180c368
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-22 21:56:39 -0600

    Refactor sessions#new action
    I pulled the boolean logic into private helper methods
    Then I re-used the redirect_logged_in method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,9 @@
 class SessionsController < ApplicationController
 
   def new
-    if current_user && current_user.regular?
-      redirect_to '/profile'
+    if already_logged_in?
       flash[:notice] = "You are already logged in."
-    elsif current_user && current_user.merchant_employee?
-      redirect_to '/merchant/dashboard'
-      flash[:notice] = "You are already logged in."
-    elsif current_user && current_user.admin?
-      redirect_to '/admin/dashboard'
-      flash[:notice] = "You are already logged in."
+      redirect_logged_in(current_user)
     end
   end
 
@@ -35,5 +29,21 @@ class SessionsController < ApplicationController
     elsif user.admin?
       redirect_to '/admin/dashboard'
     end
+  end
+
+  def regular_user_logged_in?
+    current_user && current_user.regular?
+  end
+
+  def merchant_employee_logged_in?
+    current_user && current_user.merchant_employee?
+  end
+
+  def admin_logged_in?
+    current_user && current_user.admin?
+  end
+
+  def already_logged_in?
+    regular_user_logged_in? || merchant_employee_logged_in? || admin_logged_in?
   end
 end
