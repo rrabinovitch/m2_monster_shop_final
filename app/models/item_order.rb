7f281b40b4dda76eebbd6db729cf5c4c381153ea
Lito White <leland.white@gmail.com>
2020-07-25 22:28:24 -0700

    User Story 29 Refactored

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -8,15 +8,15 @@ class ItemOrder <ApplicationRecord
     price * quantity
   end
 
-  def self.item_order_subtotal(item)
-    select_items_in_order(item).sum('price * quantity')
+  def self.select_items_in_order(item)
+    where({item_id: item.id})
   end
 
   def self.items_in_order_quantity(item)
     select_items_in_order(item).sum(:quantity)
   end
 
-  def self.select_items_in_order(item)
-    where({item_id: item.id})
+  def self.item_order_subtotal(item)
+    items_in_order_quantity(item) * item.price
   end
 end
