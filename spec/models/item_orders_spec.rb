809249e6b3d2390b4153e680e147fd313ff286c9
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-07-25 20:09:42 -0600

    Update ItemOrders spec to have a status
    ItemOrders can be unfulfilled or fulfilled

--- spec/models/item_orders_spec.rb
+++ spec/models/item_orders_spec.rb
@@ -6,11 +6,33 @@ describe ItemOrder, type: :model do
     it { should validate_presence_of :item_id }
     it { should validate_presence_of :price }
     it { should validate_presence_of :quantity }
+    it { should validate_presence_of :status }
   end
 
   describe "relationships" do
     it {should belong_to :item}
     it {should belong_to :order}
+  end
+
+  describe "status" do
+    before :each do
+      meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      tire = meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+      customer = create(:user)
+      order_1 = customer.orders.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033)
+      @unfulfilled_item_order = order_1.item_orders.create!(item: tire, price: tire.price, quantity: 2, status: 0)
+      @fulfilled_item_order = order_1.item_orders.create!(item: tire, price: tire.price, quantity: 2, status: 1)
+    end
+
+    it "can be unfulfilled" do
+      expect(@unfulfilled_item_order.status).to eq("unfulfilled")
+      expect(@unfulfilled_item_order.unfulfilled?).to be_truthy
+    end
+
+    it "can be fulfilled" do
+      expect(@fulfilled_item_order.status).to eq("fulfilled")
+      expect(@fulfilled_item_order.fulfilled?).to be_truthy
+    end
   end
 
   describe 'instance methods' do
