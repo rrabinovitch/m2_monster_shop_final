bdb82701033d910dcf0a8dab2be67e2aea38a424
Lito White <leland.white@gmail.com>
2020-07-27 07:01:28 -0700

    Test Written for user story 36

---
+++ spec/features/merchants/dashboard/item_index_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'As a merchant employee' do
+  describe 'When I visit the merchant dashboard' do
+    describe 'And click on the merchant items link' do
+    before :each do
+
+    end
+
+      it "the merchant items link takes me to the merchant items index page" do
+        
+      end
+
+
+  end
+end
