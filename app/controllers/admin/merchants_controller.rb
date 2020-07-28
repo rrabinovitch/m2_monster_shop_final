62564cdade91061850bfd6ba1a28d06d6d27a567
Judith Pillado <judith.pillado@gmail.com>
2020-07-28 14:19:49 -0500

    Add update method to merchants controller where we add flash disabled message and redirect to corresponding route

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -4,4 +4,11 @@ class Admin::MerchantsController < ApplicationController
     @merchants = Merchant.all
   end
 
+  def update
+    merchant = Merchant.find(params[:merchant_id])
+    merchant.change_status
+    flash[:disabled] = "You have now disabled #{merchant.name}"
+    redirect_to "/admin/merchants"
+  end
+
 end
