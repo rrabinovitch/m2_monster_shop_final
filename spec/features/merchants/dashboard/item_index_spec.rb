6feb079b78c761dc49a44244b6b4179104df7ddb
Lito White <leland.white@gmail.com>
2020-07-27 17:12:13 -0700

    Completed user story 36

--- spec/features/merchants/dashboard/item_index_spec.rb
+++ spec/features/merchants/dashboard/item_index_spec.rb
@@ -4,13 +4,32 @@ RSpec.describe 'As a merchant employee' do
   describe 'When I visit the merchant dashboard' do
     describe 'And click on the merchant items link' do
     before :each do
+      @merchant = create(:merchant)
+      @merchant_employee = create(:user, role: 1, merchant_id: @merchant.id)
+      visit '/login'
+      fill_in :email, with: @merchant_employee.email
+      fill_in :password, with: @merchant_employee.password
+      click_button 'Log In'
 
+      @merchant2 = create(:merchant)
+
+      @item1 = create(:item, merchant_id: @merchant.id)
+      @item2 = create(:item, merchant_id: @merchant.id)
+      @item3 = create(:item, merchant_id: @merchant.id)
+      @item4 = create(:item, merchant_id: @merchant2.id)
     end
 
       it "the merchant items link takes me to the merchant items index page" do
-        
+        visit "/merchant/dashboard"
+        expect(page).to have_link "Merchant Items"
+        click_link "Merchant Items"
+        expect(current_path).to eq("/merchant/items")
+        expect(page).to have_content(@item1.name)
+        expect(page).to have_content(@item2.name)
+        expect(page).to have_content(@item3.name)
+        expect(page).to_not have_content(@item4.name)
       end
 
-
+    end
   end
 end
