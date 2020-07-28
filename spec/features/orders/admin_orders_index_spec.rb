fe060c9df1322e43ff82f9835b7cc894a97477c6
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-07-28 08:15:45 -0600

    Outline user story 32 specs

---
+++ spec/features/orders/admin_orders_index_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'As an admin user, when I visit my admin dashboard' do
+  before :each do
+
+  end
+  
+  it "I see all orders in the system" do
+
+  end
+
+  it "For each order, I see the user who placed the order, the order id, and the date the order was created" do
+
+  end
+
+  it "For each order, the the info on the associated user links to the admin view of the user profile" do
+
+  end
+
+  it "The displayed orders are sorted by status: packaged > pending > shipped" do
+
+  end
+end
