ab4f92ea8b707a7ecaeeb675659479a6c357cb7c
Lito White <leland.white@gmail.com>
2020-07-29 19:51:02 -0700

    Update validation for item price

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,8 +8,9 @@ class Item <ApplicationRecord
                         :description,
                         :price,
                         :image,
-                        :inventory 
+                        :inventory
 
+  validates :price, :numericality => { :greater_than => 0}
   validates :inventory, :numericality => { :greater_than_or_equal_to => 0}
   validates_inclusion_of :active?, :in => [true, false]
   scope :active_items, -> {where(active?: true)}
