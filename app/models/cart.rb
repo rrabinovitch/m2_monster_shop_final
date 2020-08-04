51df3faa5cae2d6dd0534c069d84acf89c032522
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-08-04 15:28:35 -0600

    Refactor Cart#subtotal(item) and Cart#total methods

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -28,45 +28,28 @@ class Cart
   end
 
   def subtotal(item)
-    quantity = @contents[item.id.to_s]
-    discount = item.merchant.discounts.first
-    if !item.merchant.discounts.empty? && quantity >= discount.minimum_item_quantity
-      (item.price * quantity) * ((100 - discount.percentage.to_f) / 100)
-    else
-      item.price * quantity
-    end
+    item.price * @contents[item.id.to_s] if best_discount_rate(item).nil?
+    apply_discount(item)
   end
 
   def total
-    @contents.sum do |item_id, quantity|
-      item = Item.find(item_id)
-      if best_discount_rate(item).nil?
-        item.price * quantity
-      else
-        apply_discount(item)
-      end
+    total = items.sum do |item, _quantity|
+      subtotal(item)
     end
   end
 
   def available_discounts(item)
     quantity = @contents[item.id.to_s]
     item.merchant.discounts.where('minimum_item_quantity <= ?', quantity)
-
-    # available_discounts = []
-    # item.merchant.discounts.each do |discount|
-    #   available_discounts << discount if quantity >= discount.minimum_item_quantity
-    # end
-    # available_discounts
   end
 
   def best_discount_rate(item)
-    # available_discounts(item).order(percentage: :desc).first << come back to this if needing access to full discount record rather than just the percentage
     available_discounts(item).maximum(:percentage)
   end
 
   def apply_discount(item)
     quantity = @contents[item.id.to_s]
-    discount_rate = (100 - best_discount_rate(item).to_f)/100
-    (item.price * quantity) * discount_rate
+    discount_calculation = (100 - best_discount_rate(item).to_f)/100
+    (item.price * quantity) * discount_calculation
   end
 end
