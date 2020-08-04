6a4ba82cef7d35729fefbde151e4ecc24d350954
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-08-04 15:08:18 -0600

    Refactor Cart#total method and create helper methods to identify best discount

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -40,35 +40,50 @@ class Cart
   def total
     total = 0
     items.each do |item, quantity|
-      if item.merchant.discounts.empty?
+      if best_discount_rate(item).nil?
         total += (item.price * quantity)
       else
-        if quantity >= item.merchant.discounts.first.minimum_item_quantity
-          total += (item.price * quantity) * ((100 - item.merchant.discounts.first.percentage.to_f)/100)
-        else
-          total += (item.price * quantity)
-        end
+        total += apply_discount(item)
       end
     end
     total
+    # total = 0
+    # items.each do |item, quantity|
+    #   if item.merchant.discounts.empty?
+    #     total += (item.price * quantity)
+    #   else
+    #     if quantity >= item.merchant.discounts.first.minimum_item_quantity
+    #       total += (item.price * quantity) * ((100 - item.merchant.discounts.first.percentage.to_f)/100)
+    #     else
+    #       total += (item.price * quantity)
+    #     end
+    #   end
+    # end
+    # total
     # @contents.sum do |item_id,quantity|
     #   Item.find(item_id).price * quantity
     # end
   end
 
-  # def discounted_total
-  #   discounted_total = 0
-  #   items.each do |item, quantity|
-  #     if item.merchant.discounts.empty?
-  #       discounted_total += (item.price * quantity)
-  #     else
-  #       if quantity >= item.merchant.discounts.first.minimum_item_quantity
-  #         discounted_total += (item.price * quantity) * ((100 - item.merchant.discounts.first.percentage.to_f)/100)
-  #       else
-  #         discounted_total += (item.price * quantity)
-  #       end
-  #     end
-  #   end
-  #   discounted_total
-  # end
+  def available_discounts(item)
+    quantity = @contents[item.id.to_s]
+    item.merchant.discounts.where('minimum_item_quantity <= ?', quantity)
+
+    # available_discounts = []
+    # item.merchant.discounts.each do |discount|
+    #   available_discounts << discount if quantity >= discount.minimum_item_quantity
+    # end
+    # available_discounts
+  end
+
+  def best_discount_rate(item)
+    # available_discounts(item).order(percentage: :desc).first << come back to this if needing access to full discount record rather than just the percentage
+    available_discounts(item).maximum(:percentage)
+  end
+
+  def apply_discount(item)
+    quantity = @contents[item.id.to_s]
+    discount_rate = (100 - best_discount_rate(item).to_f)/100
+    (item.price * quantity) * discount_rate
+  end
 end
