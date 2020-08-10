a486c9f9ee4f1b643f0b712be4523024186d6650
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-08-10 03:02:27 -0600

    Clean up

--- config/routes.rb
+++ config/routes.rb
@@ -6,34 +6,12 @@ Rails.application.routes.draw do
   resources :merchants do
     resources :items, only: [:index, :new, :create]
   end
-  # get "/merchants", to: "merchants#index"
-  # get "/merchants/new", to: "merchants#new"
-  # get "/merchants/:id", to: "merchants#show"
-  # post "/merchants", to: "merchants#create"
-  # get "/merchants/:id/edit", to: "merchants#edit"
-  # patch "/merchants/:id", to: "merchants#update"
-  # delete "/merchants/:id", to: "merchants#destroy"
 
   resources :items, except: [:new, :create] do
     resources :reviews, only: [:new, :create]
   end
 
   resources :reviews, only: [:edit, :update, :destroy]
-  # get "/items", to: "items#index"
-  # get "/items/:id", to: "items#show"
-  # get "/items/:id/edit", to: "items#edit"
-  # patch "/items/:id", to: "items#update"
-  # get "/merchants/:merchant_id/items", to: "items#index"
-  # get "/merchants/:merchant_id/items/new", to: "items#new"
-  # post "/merchants/:merchant_id/items", to: "items#create"
-  # delete "/items/:id", to: "items#destroy"
-
-  # get "/items/:item_id/reviews/new", to: "reviews#new"
-  # post "/items/:item_id/reviews", to: "reviews#create"
-
-  # get "/reviews/:id/edit", to: "reviews#edit"
-  # patch "/reviews/:id", to: "reviews#update"
-  # delete "/reviews/:id", to: "reviews#destroy"
 
   post "/cart/:item_id", to: "cart#add_item"
   get "/cart", to: "cart#show"
@@ -43,10 +21,6 @@ Rails.application.routes.draw do
   put "/cart/:item_id", to: "cart#add_single_item"
 
   resources :orders, except: [:edit, :destroy, :index]
-  # get "/orders/new", to: "orders#new"
-  # post "/orders", to: "orders#create"
-  # get "/orders/:id", to: "orders#show"
-  # patch "/orders/:id", to: "orders#update"
 
   get "/register", to: "users#new"
   post "/users", to: "users#create"
@@ -62,13 +36,8 @@ Rails.application.routes.draw do
     get "/dashboard", to: "dashboard#index"
     get "/", to: "dashboard#index"
     resources :users, only: [:index, :show]
-    # get "/users", to: "users#index"
-    # get "/users/:id", to: "users#show"
     patch "/orders/:id", to: "orders#update"
     resources :merchants, only: [:index, :update, :show]
-    # get "/merchants", to: "merchants#index"
-    # patch "/merchants/:merchant_id", to: "merchants#update"
-    # get "/merchants/:merchant_id", to: "merchants#show"
   end
 
   namespace :merchant do
@@ -87,10 +56,6 @@ Rails.application.routes.draw do
 
   namespace :profile do
     resources :orders, only: [:index, :show, :update]
-    # get "/orders", to: "orders#index"
-    # get "/orders/:order_id", to: "orders#show"
-    # patch "/orders/:order_id", to: "orders#update"
-
     patch "/password", to: "passwords#update"
     get "/password/edit", to: "passwords#edit"
   end
