b88f6a55ba719ba569303af1da3cda67a2ee9317
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-08-10 03:33:07 -0600

    Refactor Admin Orders route

--- config/routes.rb
+++ config/routes.rb
@@ -36,7 +36,7 @@ Rails.application.routes.draw do
     get "/dashboard", to: "dashboard#index"
     get "/", to: "dashboard#index"
     resources :users, only: [:index, :show]
-    patch "/orders/:id", to: "orders#update"
+    resources :orders, only: [:update]
     resources :merchants, only: [:index, :update, :show]
   end
 
